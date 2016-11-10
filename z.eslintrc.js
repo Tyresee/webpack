73fee211d6eac8bdd2ce0b9d04250cc7bc48db2f
RainJoy1993 <1490079545@qq.com>
2016-11-10 09:34:57 +0800

    z要去掉
    z要去掉

---
+++ z.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+    extends: 'standard',
+    plugins: [
+        'html'
+    ],
+    // add your custom rules here
+    'rules': {}
+}
